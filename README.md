15583539cc3fb16cababd54f949ebe98208213dc
Benny Liebold <benny@Bennys-iMac.local>
2018-11-27 23:29:41 +0100

    added (non-working) https server

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-<h1>Simple Raspberry Pi BabyCam</h1>
+# Simple Raspberry Pi BabyCam
+
 This is a simple Raspberry Pi BabyCam.
+
+##Requirements
+
+* Raspberry Pi Zero W with OS installed on microSD card
+* Infrared Pi Camera with attached infrared LEDs
+* Node.js
+* npm
+
+## HTTPS Key and Certificate
+
+The BabyCam opens a https port and for this, you need both an SSL key and a certificate. You can generate self-signed versions of both using the following command in your terminal (Linux, MacOS) inside the ssl/ folder.
+
+```
+openssl req -nodes -new -x509 -keyout babycam.key -out babycam.cert
+```
+
+## Compiling the Code
+
+Run the following commands inside of the project's root folder in your terminal (Linux, MacOS).
+
+```
+npm install
+tsc
+```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
--- src/server.ts
+++ src/server.ts
@@ -3,18 +3,30 @@
 import express from 'express';
 import fs from 'fs';
 import https from 'https';
-import {WelcomeController} from './controllers';
+import http from 'http';
+import {WelcomeController} from './controllers'; //TODO: remove
 
 const server: express.Application = express();
-const httpPort: number = 3000;
+const httpPort: number = 8080;
 const httpsPort: number = 8443;
 
-server.use('/welcome', WelcomeController);
+const sslCredentials = {
+    key: fs.readFileSync('../ssl/babycam.key', 'utf8'),
+    certificate: fs.readFileSync('../ssl/babycam.cert', 'utf8')
+};
 
-server.listen(httpPort, () => {
-    console.log(`Listening at http://localhost:${httpPort}/`);
-});
+server.use('/welcome', WelcomeController); //TODO: swap for index.html
 
-server.listen(httpsPort, () => {
-    console.log(`Listening at http://localhost:${httpsPort}/`);
-});
+const httpServer = http.createServer(server);
+const httpsServer = https.createServer(sslCredentials, server);
+
+httpServer.listen(httpPort, () => {
+    console.log(`BabyCam (redirect) listening at http://localhost:${httpPort}/`);
+}); 
+
+httpsServer.listen(httpsPort, () => {
+    console.log(`BabyCam (SSL) listening at https://localhost.${httpsPort}/`);
+}); //TODO: won't work
+
+
+//TODO: add simple authentication - https://medium.com/@evangow/server-authentication-basics-express-sessions-passport-and-curl-359b7456003d
